d3635b37d667f3984fa851541e7cfe95265ac6af
Ahmet Kaan GÜMÜŞ <96421894+Tahinli@users.noreply.github.com>
2024-06-10 03:31:54 +0300

    feat: :sparkles: activation function and weight + bias seperated

--- src/network.rs
+++ src/network.rs
@@ -1,12 +1,26 @@
 use crate::neuron::Neuron;
+struct NeuralLine {
+    neural_line: Vec<Neuron>,
+}
+
+impl NeuralLine {
+    fn new() -> Self {
+        NeuralLine {
+            neural_line: vec![],
+        }
+    }
+
+    fn push_neuron(&mut self, neuron: Neuron) {
+        self.neural_line.push(neuron);
+    }
+}
 
 pub struct NeuralNetwork {
-    neurons: Vec<Neuron>,
+    neurons: Vec<NeuralLine>,
     pub input: f64,
     expectation: f64,
     result: f64,
 }
-
 impl NeuralNetwork {
     pub fn new() -> Self {
         NeuralNetwork {
@@ -16,14 +30,18 @@ impl NeuralNetwork {
             result: 0.0,
         }
     }
-    pub fn create_neurons(&mut self, neuron_capacity: u128) {
+    pub fn create_neurons(&mut self, neuron_capacity: u128, neuron_line_capacity: u128) {
         self.neurons.clear();
-        for _ in 0..neuron_capacity {
-            self.push_neuron(Neuron::new());
+        for _ in 0..neuron_line_capacity {
+            let mut neural_line = NeuralLine::new();
+            for _ in 0..neuron_capacity {
+                neural_line.push_neuron(Neuron::new());
+            }
+            self.push_neuron_line(neural_line);
         }
     }
-    fn push_neuron(&mut self, neuron: Neuron) {
-        self.neurons.push(neuron);
+    fn push_neuron_line(&mut self, neuron_line: NeuralLine) {
+        self.neurons.push(neuron_line);
     }
 
     pub fn run(&mut self) {
@@ -33,17 +51,11 @@ impl NeuralNetwork {
 
     fn forward(&mut self) {
         let mut input = self.input;
-        for nerve in &mut self.neurons {
-            nerve.set_variable(input);
-            input = nerve.activate();
-        }
-        self.result = input;
+        todo!()
     }
 
     fn backward(&mut self) {
         let loss = self.expectation - self.result;
-        for nerve in &mut self.neurons {
-            nerve.recalculate_coefficients(loss);
-        }
+        todo!()
     }
 }
