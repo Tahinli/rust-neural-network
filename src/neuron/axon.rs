c35fbcc4c914cd3a7ed383940efcf8228437f354
Ahmet Kaan GÜMÜŞ <96421894+Tahinli@users.noreply.github.com>
2025-02-11 17:49:18 +0300

    refactor: :recycle: much basic reLU implementation

--- src/neuron/axon.rs
+++ src/neuron/axon.rs
@@ -8,16 +8,7 @@ pub struct Axon {
 impl Axon {
     pub fn activate(&self, soma: &Soma) -> f64 {
         // ReLU
-        let soma_value = soma.get_value();
-        if soma_value > 0.0 {
-            if soma_value > self.treshold {
-                soma_value
-            } else {
-                0.0
-            }
-        } else {
-            0.0
-        }
+        soma.get_value().max(0.0)
     }
 
     pub fn set_treshold(&mut self, treshold: f64) {
